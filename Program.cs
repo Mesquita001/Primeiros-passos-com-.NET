165300207a5cd0b65a7dc8f2d18cf378b01e0977
Andre Mesquita <mesquitas@hotmail.com>
2021-09-23 22:05:02 -0300

    aprendendo .NET

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DigitalInovationOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numerDeVEzes = 5;
+            for (int i = 0; i < numerDeVEzes; i++)
+            {
+               Console.WriteLine( "Bem vindo ao curso de .NET ");
+            }
+             
+        }
+    }
+}
